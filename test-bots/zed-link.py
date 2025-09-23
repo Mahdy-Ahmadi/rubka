4e5f6d3a8362d8a79c1667c184558dbcf8fe46e6
Mahdi Ahmadi <mehdy.ahmadi.5@gmail.com>
2025-09-24 02:32:47 +0330

    Update zed-link.py

--- test-bots/zed-link.py
+++ test-bots/zed-link.py
@@ -3,7 +3,7 @@ from rubka.asynco import Robot,filters,Message
 
 bot = Robot("")
 
-@bot.on_message(filters.text_regex(r"(https?://|www\.|\.ir|\.com|\.net|t\.me|@\w+)"))
+@bot.on_message_group(filters=filters.text_regex(r"(https?://|www\.|\.ir|\.com|\.net|t\.me|@\w+)"))
 async def handle_start(bot: Robot, message: Message):
     await message.delete()
 
