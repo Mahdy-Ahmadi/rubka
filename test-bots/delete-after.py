1839e2a75ddc99c78eb2fb285bef0c54f1cc96da
Mahdi Ahmadi <mehdy.ahmadi.5@gmail.com>
2025-10-13 23:39:24 +0330

    Create delete-after.py

---
+++ test-bots/delete-after.py
@@ -0,0 +1,14 @@
+import asyncio
+from rubka.asynco import Robot,Message
+
+bot = Robot("")
+
+@bot.on_message()
+async def handle_start(bot: Robot, message: Message):
+    name = await message.name
+    await message.reply(
+        f"your Name : {name}",
+        delete_after=10
+    )
+
+asyncio.run(bot.run())
