be964f8fd9a0c67f57cee13f35a5b95b49de29ef
Mahdi Ahmadi <mehdy.ahmadi.5@gmail.com>
2025-10-21 22:38:11 +0330

    Update get_message.py

--- test-bots/get_message.py
+++ test-bots/get_message.py
@@ -4,6 +4,7 @@ from rubka.asynco import Robot, Message
 bot = Robot("")
 
 bot.start_save_message() #این متود به معنای شروع شنود پیام ها توسط ربات هست برای استفاده داخل متود گت مسیج
+# اگه بخواید یه سورس متود گت مسیج داخلش کار کنه حتما باید اول سورس این متود رو بزارید تا ربات بعد از فعال شدن شروع به شنود مکالمات کنه برای دریافت از طریق متود گت مسیج
 
 @bot.on_message()
 async def info(bot: Robot, message: Message):
