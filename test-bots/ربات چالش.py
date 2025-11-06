f84003900e4f1ce1d4cafa2903b9eb1af5deac01
Mahdi Ahmadi <mehdy.ahmadi.5@gmail.com>
2025-11-07 01:05:55 +0330

    Update ربات چالش.py

--- test-bots/ربات چالش.py
+++ test-bots/ربات چالش.py
@@ -1,25 +1,35 @@
-import asyncio
-import requests
-from rubka.asynco import Robot, Message, filters,InlineBuilder
+import aiohttp
+from rubka.asynco import Robot, Message, filters
 
-bot = Robot("token")
+bot = Robot(
+    "token",
+    safeSendMode=True
+)
 
-@bot.on_message(filters.text_equals("چالش"))
-async def handle_start(bot: Robot, message: Message):
-    response = requests.get("https://api.rubka.ir/poll")
-    poll_data = response.json()
-    inline = (InlineBuilder().row(InlineBuilder().button_simple("0", "ربات چالش آنلاین")).build()) if message.is_private else None
-    await bot.send_poll(
-        message.chat_id,
-        reply_to_message_id=message.message_id,
-        question=poll_data['question'],
-        options=poll_data['options'],
-        type=poll_data['type'],
-        allows_multiple_answers=poll_data['allows_multiple_answers'],
-        is_anonymous=poll_data['is_anonymous'],
-        correct_option_index= poll_data['correct_option_index'],
-        hint=poll_data.get('hint'),
-        show_results=poll_data['show_results'],
-        inline_keypad=inline
-    )
-asyncio.run(bot.run())
+async def fetch_poll():
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://api.rubka.ir/poll", timeout=10) as response:
+            return await response.json()
+
+@bot.on_message(filters.text_contains_any(["چالش","کوییز","Quiz"]))
+async def handle_challenge(bot: Robot, message: Message):
+    sent = await message.reply("درحال دریافت اطلاعات چالش لطفا منتظر باشید...")
+    try:
+        poll_data = await fetch_poll()
+
+        await bot.send_poll(
+            chat_id=message.chat_id,
+            question=poll_data["question"],
+            options=poll_data["options"],
+            type="Quiz",
+            hint=poll_data.get("hint", ""),
+            correct_option_index=0,
+            reply_to_message_id=message.message_id
+        )
+
+        await sent.delete()
+    except Exception as e:
+        await sent.delete()
+        await message.reply(f"Error {e}")
+
+bot.run()
