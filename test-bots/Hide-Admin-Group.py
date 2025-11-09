f509cb64af5a055f7e46eb082914536fbf9d347a
Mahdi Ahmadi <mehdy.ahmadi.5@gmail.com>
2025-11-09 19:12:29 +0330

    Update Hide-Admin-Group.py

--- test-bots/Hide-Admin-Group.py
+++ test-bots/Hide-Admin-Group.py
@@ -1,28 +1,33 @@
-from rubka.asynco import Robot, Message,filters
+from rubka.asynco import Robot, Message, filters
 
-ADMIN_ID = "" #سندر ایدی ادمین ربات
-TOKEN = "" # توکن شما
+ADMIN_ID = "u0HJtFl043f3d7e43bdc1f305ed42a11" #سندر ایدی مالک گروه
+TOKEN = "" # توکن ربات
 
 bot = Robot(TOKEN)
 
 @bot.on_message(filters.is_group)
 async def handle_admin_message(bot: Robot, message: Message):
-    if message.sender_id != ADMIN_ID:return
-    await message.delete()
-    if message.is_photo:
-        file_url = await bot.get_url_file(message.file.file_id)
-        await bot.send_image(
-            message.chat_id,
-            file_url,
-            text=message.text,
-            reply_to_message_id=message.reply_to_message_id or None
-        )
+    if message.sender_id != ADMIN_ID:
         return
-    if message.text:
-        await bot.send_message(
-            message.chat_id,
-            message.text,
-            reply_to_message_id=message.reply_to_message_id or None
-        )
-
+    try:
+        send_methods = {
+            "is_photo": bot.send_image,
+            "is_video": bot.send_video,
+            "is_document": bot.send_document,
+            "is_text": bot.send_message
+        }
+        for attr, send_func in send_methods.items():
+            if getattr(message, attr, False):
+                await message.delete()
+                kwargs = {
+                    "chat_id": message.chat_id,
+                    "text": message.text,
+                    "reply_to_message_id": message.reply_to_message_id or None
+                }
+                if hasattr(message, "file") and message.file:
+                    kwargs["file_url"] = await bot.get_url_file(message.file.file_id)
+                await send_func(**kwargs)
+                break
+    except Exception as e:
+        print(e)
 bot.run()
