31d7193e169b1bde661c45840869413d4e6e1e49
Mahdi Ahmadi <mehdy.ahmadi.5@gmail.com>
2025-11-09 04:36:22 +0330

    Create Hide-Admin-Group.py

---
+++ test-bots/Hide-Admin-Group.py
@@ -0,0 +1,28 @@
+from rubka.asynco import Robot, Message
+
+ADMIN_ID = "" #سندر ایدی ادمین ربات
+TOKEN = "" # توکن شما
+
+bot = Robot(TOKEN)
+
+@bot.on_message()
+async def handle_admin_message(bot: Robot, message: Message):
+    if message.sender_id != ADMIN_ID:return
+    await message.delete()
+    if message.is_photo:
+        file_url = await bot.get_url_file(message.file.file_id)
+        await bot.send_image(
+            message.chat_id,
+            file_url,
+            text=message.text,
+            reply_to_message_id=message.reply_to_message_id or None
+        )
+        return
+    if message.text:
+        await bot.send_message(
+            message.chat_id,
+            message.text,
+            reply_to_message_id=message.reply_to_message_id or None
+        )
+
+bot.run()
