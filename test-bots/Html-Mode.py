7fcb63bcbdd747b84f11787df281d852eec2476f
Mahdi Ahmadi <mehdy.ahmadi.5@gmail.com>
2025-11-10 01:35:47 +0330

    Create Html-Mode.py

---
+++ test-bots/Html-Mode.py
@@ -0,0 +1,22 @@
+from rubka.asynco import Robot, Message
+
+TOKEN = ""
+bot = Robot(TOKEN)
+
+@bot.on_message()
+async def handle_intro(bot: Robot, message: Message):
+    html_text = """
+<b>Hello everyone!</b> 👋<br>
+<u>This is a complete guide message</u> that includes all formatting:<br>
+<b>Bold text</b><br>
+<i>Italic text</i><br>
+<u>Underlined text</u><br>
+<s>Strikethrough text</s><br>
+<code>Inline code</code><br>
+<a href="https://rubka.ir">Hyperlink to Rubka</a><br>
+Emojis 😎✨🔥<br><br>
+For more information, visit <a href="https://rubka.ir">Rubka website</a>.
+"""
+    await message.reply(html_text, parse_mode="HTML")
+
+bot.run()
