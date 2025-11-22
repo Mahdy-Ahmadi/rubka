2685f2d52c9af2e9ceb21b89b4c345f5227906cb
Mahdi Ahmadi <mehdy.ahmadi.5@gmail.com>
2025-11-22 14:58:35 +0330

    Create down-music.py

---
+++ test-bots/down-music.py
@@ -0,0 +1,28 @@
+import asyncio
+from rubka.asynco import Robot
+from rubka.context import Message
+
+bot = Robot("token",show_progress=True)
+
+@bot.on_message()
+async def handle_message(bot: Robot, message: Message):
+    if not message.is_audio:
+        await message.reply("لطفاً یک موزیک ارسال کنید.")
+        return
+    try:
+        file_path = "name.mp3"
+        downloaded = await bot.download(message.file.file_id, file_path)
+        
+        if downloaded:
+            sent = await bot.send_music(
+                chat_id=message.chat_id,
+                path=file_path,
+                text="عه 🎵",
+                file_name="music.mp3",
+                reply_to_message_id=message.message_id
+            )
+        else:
+            await message.reply("❌ دانلود فایل با مشکل مواجه شد.")
+    except Exception as e:
+        await message.reply(f"خطا هنگام پردازش فایل:\n{str(e)}")
+asyncio.run(bot.run())
