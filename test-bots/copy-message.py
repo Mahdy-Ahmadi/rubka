ac2c7527d0175f37a3d6031fd1cc910b7e221f52
Mahdi Ahmadi <mehdy.ahmadi.5@gmail.com>
2025-11-26 11:55:58 +0330

    Update copy-message.py

--- test-bots/copy-message.py
+++ test-bots/copy-message.py
@@ -1,8 +1,8 @@
 from rubka.asynco import Robot, Message, filters
 
-ADMIN_ID = "u0HJtFl043f3d7e43bdc1f305ed42a11" #سندر ایدی ادمین گپ
+ADMIN_ID = "" #سندر ایدی ادمین گپ
 
-bot = Robot("BBIDF0SPZDZOLGSCIFEVSBAZAWCYXLCLOEGBWPDLKFTRAABEOOXZDLVCMMLLGCQM",api_endpoint="messenger")
+bot = Robot("token",api_endpoint="messenger")
 
 @bot.on_message(filters.sender_id_is(ADMIN_ID))
 async def handle_admin_message(bot: Robot, message: Message):
